3ba46bab206bfc44bba36b06367ad2701f48eb50
Maiko <maikossmaster@gmail.com>
2018-06-28 10:41:04 -0300

    Configurando loading no saga.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,27 @@
 import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as FavoriteActions from "../../store/actions/favorites";
+import { spawn } from "child_process";
 
 class Main extends Component {
+  static propTypes = {
+    addFavoriteRequest: PropTypes.func.isRequired,
+    favorites: PropTypes.shape({
+      loading: PropTypes.bool,
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          name: PropTypes.string,
+          description: PropTypes.string,
+          url: PropTypes.string
+        })
+      )
+    }).isRequired
+  };
+
   state = {
     repositoryInput: ""
   };
@@ -12,6 +29,7 @@ class Main extends Component {
   handleAddRepository = event => {
     event.preventDefault();
     this.props.addFavoriteRequest(this.state.repositoryInput);
+    this.setState({ repositoryInput: "" });
   };
 
   render() {
@@ -24,10 +42,11 @@ class Main extends Component {
             onChange={e => this.setState({ repositoryInput: e.target.value })}
           />
           <button type="submit">Adicionar</button>
+          {this.props.favorites.loading && <span>Carregando...</span>}
         </form>
 
         <ul>
-          {this.props.favorites.map(favorite => (
+          {this.props.favorites.data.map(favorite => (
             <li key={favorite.id}>
               <p>
                 <strong>{favorite.name}</strong>({favorite.description})
