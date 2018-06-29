34d4077008c4835c9d08213fb3114167d332e856
Maiko <maikossmaster@gmail.com>
2018-06-29 13:46:31 -0300

    Duck Pattern.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,8 +3,7 @@ import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import * as FavoriteActions from "../../store/actions/favorites";
-import { spawn } from "child_process";
+import { Creators as FavoriteActions } from "../../store/ducks/favorites";
 
 class Main extends Component {
   static propTypes = {
